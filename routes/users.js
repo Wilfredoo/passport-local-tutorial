f33aeef9a8ac49685624a0d57eabcedb8cee3abe
Wilfredo <casas.farach@gmail.com>
2020-03-25 12:20:53 +0100

    displaying error messages in form, hashing password

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
+const User = require("../models/User");
+
+// routes
 // router.get("/dashboard", (req, res) => res.render("dashboard"));
-
 router.get("/login", (req, res) => res.render("login"));
-
 router.get("/register", (req, res) => res.render("register"));
 
 // register handle
@@ -24,8 +26,34 @@ router.post("/register", (req, res) => {
     errors.push({ msg: "password should be at least 6 characters" });
   }
 
-  console.log("eooo", req.body);
-  res.send("hwlloi");
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    // validation passes
+    // first we check if user exists or not
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: "email is already registered" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({ name, email, password });
+        console.log(newUser);
+        res.send("oink");
+      }
+    });
+  }
 });
 
 module.exports = router;

8d53195a1e03e69ac91a54730662106ff4e0059d
Wilfredo <casas.farach@gmail.com>
2020-03-25 19:57:28 +0100

    added passport code in users.js and login route in app.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
 
 const User = require("../models/User");
 
@@ -65,6 +66,15 @@ router.post("/register", (req, res) => {
       }
     });
   }
+});
+
+// login handle
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })(req, res, next);
 });
 
 module.exports = router;
